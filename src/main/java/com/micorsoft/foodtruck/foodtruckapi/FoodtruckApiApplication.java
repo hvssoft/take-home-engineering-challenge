ea24fbad826899405ab8fe2a39b926f469a96391
Hari Sivaprasad <sivaprasad.hari@gmail.com>
2022-01-03 17:06:06 -0500

    added missing class fixed the author

---
+++ src/main/java/com/micorsoft/foodtruck/foodtruckapi/FoodtruckApiApplication.java
@@ -0,0 +1,13 @@
+package com.micorsoft.foodtruck.foodtruckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtruckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtruckApiApplication.class, args);
+	}
+
+}
